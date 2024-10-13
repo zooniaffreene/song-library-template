e54e18845b8d0ceec25db8750663aedeb98899a4
Mengjiao Cyr <mcyr@pleasantonusd.net>
2024-10-13 04:51:37 +0000

    Fixed typo

--- src/App.java
+++ src/App.java
@@ -6,6 +6,8 @@ public class App {
         System.out.println(s);
 
         ArrayList<Song> songs = SongImporter.importSongsFromCSV("songs.csv");
+
+        System.out.println("\n Example from returned songs ArrayList:");
         System.out.println(songs.get(2));
     }
 }
