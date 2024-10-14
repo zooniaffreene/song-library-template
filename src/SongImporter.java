02c9a758aaf7e72556fc71a66d62ac0b7c3d31b8
Mengjiao Cyr <mcyr@pleasantonusd.net>
2024-10-14 04:41:34 +0000

    Simlified splitting lines.

--- src/SongImporter.java
+++ src/SongImporter.java
@@ -16,13 +16,13 @@ public class SongImporter
             // First line is a header
             // artist_name, track_name, release_date, genre, len, shake the audience, obscene, danceability, loudness, topic
             String header_fields = csv.nextLine();
-            List<String> items = Arrays.asList(header_fields.split(","));
             
             while (csv.hasNextLine()) {
                 String line = csv.nextLine();
-                List<String> fields = Arrays.asList(line.split(","));
+                String[] fields = line.split(",");
                 // Construct a song object using the fields.
-                System.out.println("Field 0: " + fields.get(0));
+                System.out.println("Field 0: " + fields[0]);
+                // Replace the next line with your song constructor
                 Song song = new Song(); // code to construct a song object
                 songs.add(song);
             }
