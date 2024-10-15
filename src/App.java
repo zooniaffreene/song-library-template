8d497b5d4538c960053fb5d4599d0b07d4c8465a
Mengjiao Cyr <mcyr@pleasantonusd.net>
2024-10-15 05:26:46 +0000

    Using next

--- src/App.java
+++ src/App.java
@@ -2,13 +2,14 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Song s = new Song();
-        System.out.println(s);
-
+        
         ArrayList<Song> songs = SongImporter.importSongsFromCSV("songs.csv");
 
-        System.out.println("\n Example from returned songs ArrayList:");
-        System.out.println(songs.get(2));
+        for (int i=0; i<songs.size(); i++)
+        {
+            Song s = songs.get(i);
+            System.out.println("Artist:" + s.getArtist());
+        }       
     }
 }
 
--- src/SongImporter.java
+++ src/SongImporter.java
@@ -3,31 +3,52 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class SongImporter
 {
     public static ArrayList<Song> importSongsFromCSV(String filename)
     {
         ArrayList<Song> songs = new ArrayList<Song>();
+        int lineNumber = 0;
         try {
             File file = new File(filename);
             Scanner csv = new Scanner(file);
+            csv.useDelimiter(",");
 
             // First line is a header
             // artist_name, track_name, release_date, genre, len, shake the audience, obscene, danceability, loudness, topic
             String header_fields = csv.nextLine();
-            
+
             while (csv.hasNextLine()) {
-                String line = csv.nextLine();
-                String[] fields = line.split(",");
-                // Construct a song object using the fields.
-                System.out.println("Field 0: " + fields[0]);
-                // Replace the next line with your song constructor
-                Song song = new Song(); // code to construct a song object
-                songs.add(song);
+                try {
+                    lineNumber++;
+                    String artistName = csv.next();
+                    String trackName = csv.next();
+                    String releaseDate = csv.next();
+                    String genre = csv.next();
+                    String length = csv.next();
+                    String shakeTheAudience = csv.next();
+                    String obscene = csv.next();
+                    String danceability = csv.next();
+                    String loudness = csv.next();
+                    String topic = csv.next();
+
+                    // Debug print
+                    //System.out.println("length: " + length + "topic: " + topic);
+
+                    // Replace the next line with your song constructor
+                    Song song = new Song(artistName, trackName); // code to construct a song object
+                    songs.add(song);
+                }
+                catch (NoSuchElementException ex)
+                {
+                    System.out.println("Exception processing line: " + lineNumber);
+                }
             }
             csv.close();
         } catch (Exception ex) {
+            System.out.println("Error on line: " + lineNumber);
             ex.printStackTrace();
         }
         return songs;
