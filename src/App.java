056fb2e6c9a572a26cd42a37eb9a645a1caea0e6
Mengjiao Cyr <mcyr@pleasantonusd.net>
2024-10-16 04:26:35 +0000

    Added comments

--- src/App.java
+++ src/App.java
@@ -1,13 +1,22 @@
 import java.util.ArrayList;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        
-        ArrayList<Song> songs = SongImporter.importSongsFromCSV("songs.csv");
+    public static void main(String[] args)
+    {
+        // Variable to store the songs
+        ArrayList<Song> songs;
 
+        // import the songs from the CSV using the SongImporter
+        songs = SongImporter.importSongsFromCSV("songs.csv");
+
+        // Loop across all the songs imported
         for (int i=0; i<songs.size(); i++)
         {
+            // Get the current song matching the loop index i
             Song s = songs.get(i);
+
+            // Example - print the song
+            // You should add code to compute song statistics here.
             System.out.println("Artist:" + s.getArtist());
         }       
     }
--- src/SongImporter.java
+++ src/SongImporter.java
@@ -7,29 +7,42 @@ import java.util.NoSuchElementException;
 
 public class SongImporter
 {
+    // SongImporter does not store any data, use "static"
+    // This method opens the file given by the parameter filename.
+    // Then it reads the data and breaks the lines up based on commas into a series of fields.
+    // Those fields are then used to create Song objects
     public static ArrayList<Song> importSongsFromCSV(String filename)
     {
         ArrayList<Song> songs = new ArrayList<Song>();
         int lineNumber = 0;
+
+        // The code in this "try" block will run. Any errors will get handled by the "catch" below. 
         try {
+            // Create a File object from the filename
             File file = new File(filename);
+            
+            // Create a Scanner object, this will read from the file.
             Scanner csv = new Scanner(file);
-            csv.useDelimiter(",");
 
-            // First line is a header
+            // First line is a header - print it to see what it looks like.
             // artist_name, track_name, release_date, genre, len, shake the audience, obscene, danceability, loudness, topic
-            String header_fields = csv.nextLine();
+            String headerFields = csv.nextLine();
+            System.out.println("Header fields: " + headerFields);
 
-            while (csv.hasNextLine()) {
+            // This while loop will loop until there are no more lines left in the CSV file.
+            while (csv.hasNextLine()) 
+            {
+                lineNumber++; // Keep track of line numbers to make inspection of data errors simpler.
+
+                // Limit to 30 lines for testing (remove when you are testing real files)
                 if (lineNumber > 30)
                 {
                     break;
                 }
 
-                lineNumber++; // Keep track of line numbers to make inspection of data errors simpler.
-
-                // This puts the next line of the file into a String named line.
+                // Read one line from the CSV file and store it in the variable "line"
                 String line = csv.nextLine();
+
                 // The string is then split on each comma into an array of fields.
                 String[] fields = line.split(",");
 
@@ -51,11 +64,12 @@ public class SongImporter
                 // Replace the next line with your song constructor
                 Song song = new Song(artistName, trackName); // code to construct a song object
 
-                // Add the song object to the data output.
+                // Add the song object to the data output. (Leave alone!~)
                 songs.add(song);
             }
             csv.close();
         } catch (Exception ex) {
+            // If there is an error, this code will run
             System.out.println("Error on line: " + lineNumber);
             ex.printStackTrace();
         }
