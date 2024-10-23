b246e7fe7d2da4e3a219751548425c235708c107
zooniaffreene <zooniaffreene1073@gmail.com>
2024-10-23 14:19:28 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -6,19 +6,20 @@ public class App {
         // Variable to store the songs
         ArrayList<Song> songs;
 
-        // import the songs from the CSV using the SongImporter
+        // Import the songs from the CSV using the SongImporter
         songs = SongImporter.importSongsFromCSV("songs.csv");
 
         // Loop across all the songs imported
-        for (int i=0; i<songs.size(); i++)
+        for (int i = 0; i < songs.size(); i++)
         {
             // Get the current song matching the loop index i
             Song s = songs.get(i);
 
-            // Example - print the song
-            // You should add code to compute song statistics here.
-            System.out.println("Artist:" + s.getArtist());
+            // Print all details of the song using toString()
+            System.out.println(s.toString());
+            System.out.println("------------------------------------");
         }       
     }
 }
+
 
